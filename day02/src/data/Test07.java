46d1e26f08ffc6eb0a6736842883defaa5a59c04
wijaemin <minialp2002@naver.com>
2023-06-20 13:36:43 +0900

    버스 요금표와 요금 계산

---
+++ day02/src/data/Test07.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Test07 {
+
+	public static void main(String[] args) {
+		int adult = 1200;
+		int youth = 1000;
+		int child = 500;
+		int adultCount =2;
+		int childCount =1;
+		int adultTotal=adult*adultCount;
+		int childTotal=child*childCount;
+		int total= adultTotal +childTotal;
+		
+		
+		System.out.println(adultTotal);
+		System.out.println(childTotal);
+		System.out.println(total);
+		
+	}
+
+}
