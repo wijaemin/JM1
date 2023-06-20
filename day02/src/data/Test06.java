c0c361f2ef9ec04860ca503ec87900de9b336832
wijaemin <minialp2002@naver.com>
2023-06-20 13:35:58 +0900

    KH영화관의 연령별 요금

---
+++ day02/src/data/Test06.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		int adult = 15000;
+		int youth = 12000;
+		int child = 5000;
+		int adultCount =2;
+		int youthCount =2;
+		int adultTotal=adult*adultCount;
+		int youthTotal=youth*youthCount;
+		int total= adultTotal +youthTotal;
+		
+		
+		System.out.println(adultTotal);
+		System.out.println(youthTotal);
+		System.out.println(total);
+		
+	}
+
+}
