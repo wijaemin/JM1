5e874fafc2f8defbf3720d40fd887fd2aaeed9aa
wijaemin <minialp2002@naver.com>
2023-06-21 20:16:25 +0900

    7번 과제

--- day03/src/date3/Test07.java
+++ day03/src/date3/Test07.java
@@ -9,34 +9,14 @@ public class Test07 {
 	 * 100의 배수인 해는 4의 배수여도 윤년이 아닌걸로 간주
 	 * 400의 배수인 해는 100의 배우여도 윤년으로 간주
 	 */
-		int year=2100;
-		//4의 배수인 해 year%4==0
-		//100의 배수인 해 year%100 ==0
-		//400의 배수인 해 year%400==0
-		//true일때 윤년인걸로
+		int year=1800;
 		boolean fourY= year%4 ==0;
-		boolean hundY=year%100==0;
+		boolean hundY=year%100!=0;
 		boolean fHundY=year%400==0;
 		//leap =윤년
-		boolean leap =(fHundY&&(fourY&&hundY))||((fHundY||hundY)||fourY);
+		boolean leap =fourY&&(fHundY||hundY);
+		
 		System.out.println(leap);
-		/*400=1이면 무조건 1
-		100= 400이 0이고 100이 1이면 0
-				4= 400이 1이면 무조건 1, 400이 0이고 100이 1이면 0,
-					400이랑 100이 0이고 4가 1이면 1
-					
-					
-					
-					
-					
-					
-		*/
-			
-				
-		
-		
-		
-		
 	}
 
 }
