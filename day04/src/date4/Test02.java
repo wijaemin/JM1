c775e5d46ddb26af0fc65fba8381f83ad06e7e0d
wijaemin <minialp2002@naver.com>
2023-06-22 14:08:33 +0900

    Scanner 써보기

--- day04/src/date4/Test02.java
+++ day04/src/date4/Test02.java
@@ -1,5 +1,19 @@
 package date4;
 
 public class Test02 {
-
+	public static void main(String[] args) {
+	/*
+	 * 문자열(String)
+	 * 문자가 모여있는 형태의 데이터
+	 * 참조형 데이터(reference type)
+	 * 주문제작 방식으로 처리된다(복잡한 데이터)
+	 * 참조변수(ex : 리모컨)가 존재하며 기능(버튼)이 제공된다
+	 */
+	
+	
+	String a = "hello";
+	System.out.println(a);
+		
+	}
+	
 }
