3ec12610affdb61088a37e6fc9dc1da4b36cd128
wijaemin <minialp2002@naver.com>
2023-06-23 17:38:45 +0900

    반복(loop)

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//반복(loop)
+		//-(Q)hello를 화면에 10번 출력하세요
+		int arr[] = {1,2,3,4,5,6,7,8,9,10};
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("Hello");
+		}
+		
+		
+		//for(int i=0;i<10;i++) {
+		//	System.out.println("Hello");
+		//}
+		
+	}
+}
