3f76dd08f2b92c35455531f4bd0704feee7c4532
wijaemin <minialp2002@naver.com>
2023-06-23 18:22:24 +0900

    구구단 출력

---
+++ day05/src/loop/Test05.java
@@ -0,0 +1,23 @@
+package loop;
+
+public class Test05 {
+
+	public static void main(String[] args) {
+		
+		int num=1;
+		
+		for(int i=0;i<99;i++) {
+			if(num%10==3 || num%10==6 || num%10==9) {
+				System.out.println(num);
+			}
+			if(num/10==3 || num/10==6 || num/10==9) {
+				if(num%10!=3 || num%10!=6 || num%10!=9) {
+				System.out.println(num);
+				}
+			}
+			num++;
+		}
+		
+		
+	}
+}
