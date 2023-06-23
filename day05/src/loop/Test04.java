93e16f8d8a7a40b230f1f1708cce27c975281ebc
wijaemin <minialp2002@naver.com>
2023-06-23 20:13:19 +0900

    과제 풀이

--- day05/src/loop/Test04.java
+++ day05/src/loop/Test04.java
@@ -3,25 +3,18 @@ package loop;
 public class Test04 {
 
 	public static void main(String[] args) {
-		int num=1;
+	int num=1;
 		
-		for(int i=0;i<99;i++) {
-				if(num/10==5) {
-					System.out.println(num);
-					
-				}
-				if(num%10==5) {
-					if(num/10!=5) {
-						System.out.println(num);	
-					}
-					
-				}
-			num++;
+	for(int i=0;i<99;i++) {
+		if(num/10==5) {
+			System.out.println(num);
 		}
-		
-		
-		
-		
-		
+		if(num%10==5) {
+			if(num/10!=5) {
+				System.out.println(num);	
+				}
+		}
+		num++;
+		}
 	}
 }
--- day05/src/loop/Test05.java
+++ day05/src/loop/Test05.java
@@ -16,8 +16,6 @@ public class Test05 {
 				}
 			}
 			num++;
-		}
-		
-		
+		}	
 	}
 }
