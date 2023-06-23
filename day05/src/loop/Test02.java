0164f03d4b7aa5a1e9431b7932da3f3dde3919c6
wijaemin <minialp2002@naver.com>
2023-06-23 17:39:04 +0900

    숫자 5번 입력받는 프로그램

---
+++ day05/src/loop/Test02.java
@@ -0,0 +1,16 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		for(int i =0;i<5;i++) {
+			int num =sc.nextInt();
+		}
+		sc.close();
+	}
+	
+}
