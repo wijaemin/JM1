d7302fc69b37941b73b4e1e8988ce83a186d1279
wijaemin <minialp2002@naver.com>
2023-06-26 13:13:23 +0900

    백준 풀이

--- BAEKJOON/src/Main.java
+++ BAEKJOON/src/Main.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
 public class Main {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		
-		int A =sc.nextInt();
-		int B =sc.nextInt();
-		int C =sc.nextInt();
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int C=sc.nextInt();
 		
-		System.out.println((A+B)%C);
-		System.out.println(((A%C) + (B%C))%C);
-		System.out.println((A*B)%C);
-		System.out.println( ( (A%C) * (B%C) )%C);
-	
-		sc.close();	
+		int result=A+B+C;
+		System.out.println(result);
+		sc.close();
 	}
 }
