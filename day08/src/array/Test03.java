3f0e18abcec36bf6871e3c996bf39c0bf03b0de7
wijaemin <minialp2002@naver.com>
2023-06-28 10:09:38 +0900

    배열 다루기

---
+++ day08/src/array/Test03.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		/*	'1.5' , '2.4' , '3.6'을 저장하기 위한 배열을 생성하고
+		 * 데이터를 저장한 뒤 출력하세요
+		 * 
+		 */
+		float[] f =new float[3];
+		
+		f[0]=1.5f;
+		f[1]=2.4f;
+		f[2]=3.6f;
+		System.out.println(f[0]);
+		System.out.println(f[1]);
+		System.out.println(f[2]);
+		
+		
+	}
+
+}
---
+++ day08/src/array/Test04.java
@@ -0,0 +1,23 @@
+package array;
+
+public class Test04 {
+	public static void main(String[]args) {
+		/* '자바','파이썬','루비'를 저장하기 위한 배열을 생성하고
+		 * 데이터를 저장한 뒤 출력하세요
+		 * 
+		 */
+		String[] s = new String[3];
+		
+		s[0]="자바";
+		s[1]="파이썬";
+		s[2]="루비";
+		
+		System.out.println(s[0]);
+		System.out.println(s[1]);
+		System.out.println(s[2]);
+		
+		
+		
+		
+	}
+}
