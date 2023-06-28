c057ff03649ce5017563a83b3eb69dc332fde950
wijaemin <minialp2002@naver.com>
2023-06-28 16:20:08 +0900

    배열 최대 최소 찾기

--- day08/src/array/Test10.java
+++ day08/src/array/Test10.java
@@ -9,22 +9,22 @@ public class Test10 {
 		 * 		
 		 */
 		int[] data =new int[] {30,50,20,10,90};
+		
 		//data ========>[30],[50],[20],[10],[40] (length=5)
 		//최대값은 [0]위치가 가장 크다고 가정하고[1]부터 끝까지 비교하여 더 큰 데이터가 나오면 덮어쓰기하여 구한다
-		int max=data[0];
+		int maxIndex=0;
 		for(int i=1;i<data.length;i++) {
-			System.out.println("현재 max 값= "+max);
-			if(max<data[i]) {
-				max=data[i];
+			if(data[maxIndex]<data[i]) { //더 큰 숫자가 나오면
+				maxIndex=i; //위치를 갈아엎는다
 			}
-			System.out.println("바뀐 max 값= "+max);
-			System.out.println("================");
+			
 			
 		}
 		
 		
 		
-		
+		System.out.println("최대값 위치 = " + maxIndex);
+		System.out.println("최대값 = " + data[maxIndex]);
 	}
 
 }
