b843cab954e3992767014c6b8152353e5eb232ec
wijaemin <minialp2002@naver.com>
2023-06-29 10:04:03 +0900

    같은 자리 나오면 다시

---
+++ day08/src/array/Test14_1.java
@@ -0,0 +1,40 @@
+package array;
+
+import java.util.Random;
+
+public class Test14_1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1.모든 자리는 1번씩 다른 위치와 바뀌어야 한다
+		 * 2.어디랑 섞일지 대상 위치는 모르며 같은 위치일 수도 있다
+		 * 3.랜덤으로 정해진 위치와 현재 위치의 데이터를 바꾸도록 구현
+		 * 
+		 */
+		Random r= new Random();
+		int[]num=new int[] {1,2,3,4,5,6,7};//[0],[1],[2],[3],[4][5],[6] length=7
+		
+		
+		for(int i=0;i<num.length;i++) {
+		int random=r.nextInt(num.length);//0,1,2,3,4,5,6	
+		
+		if(i==random) { //같은 자리면 다시
+			i--;
+			continue;
+		}
+	
+		int backup=num[i];
+		num[i]=num[random];
+		num[random]=backup;
+		
+		System.out.println(num[0]);
+		System.out.println(num[1]);
+		System.out.println(num[2]);
+		System.out.println(num[3]);
+		System.out.println(num[4]);
+		System.out.println(num[5]);
+		System.out.println(num[6]);
+		System.out.println("=================");
+		}
+	}
+}
