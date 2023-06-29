104bec04d5914dee782bc83097b606c92326b3ed
wijaemin <minialp2002@naver.com>
2023-06-29 15:07:51 +0900

    2차워 배열

---
+++ day09/src/array3/Test03.java
@@ -0,0 +1,20 @@
+package array3;
+
+public class Test03 {
+	public static void main(String[] args) {
+		//다음 데이터를 2차월 배열에 저장하고 출력
+		float[][]num=new float[2][3];
+		num[0][0]=1.5f;
+		num[0][1]=2.5f;
+		num[0][2]=3.6f;
+		num[1][0]=10.2f;
+		num[1][1]=5.3f;
+		num[1][2]=6.5f;
+		System.out.println(num[0][0]);
+		System.out.println(num[0][1]);
+		System.out.println(num[0][2]);
+		System.out.println(num[1][0]);
+		System.out.println(num[1][1]);
+		System.out.println(num[1][2]);
+	}
+}
