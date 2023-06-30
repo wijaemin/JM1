04eecc5c92542779ed8ad1401a33f47e0cb5cda0
wijaemin <minialp2002@naver.com>
2023-06-30 14:17:26 +0900

    요금제

--- day10/src/oop/method3/Tariff.java
+++ day10/src/oop/method3/Tariff.java
@@ -1,6 +1,7 @@
 package oop.method3;
 
 public class Tariff {
+	//멤버 필드
 	String company;
 	String product;
 	int price;
@@ -8,7 +9,7 @@ public class Tariff {
 	int call;
 	int message;
 	
-	//초기화용
+	//멤버 메소드
 	void setup(String company,String product,int price,int data, int call, int message) {
 		this.company=company;
 		this.product=product;
@@ -20,6 +21,7 @@ public class Tariff {
 	//출력용
 	
 	void show() {
+		System.out.println("<요금제 정보>");
 		System.out.println("통신사 : " + company);
 		System.out.println("상품명 : " + product);
 		System.out.println("월정액 : " + price);
--- day10/src/oop/method3/Test01.java
+++ day10/src/oop/method3/Test01.java
@@ -3,14 +3,19 @@ package oop.method3;
 public class Test01 {
 
 	public static void main(String[] args) {
+		//객체 생성
 		Tariff t1= new Tariff();
 		Tariff t2= new Tariff();
 		Tariff t3= new Tariff();
 		
 		
+		//데이터 초기화
+		
 		t1.setup("SK", "5G언텍트52", 52000, 200, 1000, 2000);
 		t2.setup("KT", "5G세이브", 45000, 100, 900, 1500);
 		t3.setup("LG", "5G시그니처", 130000, 500, 2000, 2500);
+		
+		//데이터 출력
 		
 		t1.show();
 		System.out.println("\n");
