f450418992d1fd8be9cb3e97b0cdb62d9c3d929d
wijaemin <minialp2002@naver.com>
2023-06-30 16:17:25 +0900

    오타 수정

--- day10/src/oop/method5/Test01.java
+++ day10/src/oop/method5/Test01.java
@@ -13,7 +13,7 @@ public class Test01 {
 		i1.setup("참이슬후레쉬","주류", 1200,true,true);
 		i2.setup("클라우드맥주","주류", 3000,false,true);
 		i3.setup("바나나킥","과자", 1500,false,true);
-		i4.setup("허니버터침","거ㅣ지", 2000,true,false);
+		i4.setup("허니버터침","과자", 2000,true,false);
 		
 		i1.show();
 		System.out.println("\n");
