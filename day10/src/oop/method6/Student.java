0ccdfdebc8bf0d389be4bfd861937882c31b7ba0
wijaemin <minialp2002@naver.com>
2023-06-30 17:10:19 +0900

    학생 성적 정보

--- day10/src/oop/method6/Student.java
+++ day10/src/oop/method6/Student.java
@@ -8,23 +8,16 @@ public class Student {
 	int korean;
 	int english;
 	int math;
-	int total;
-	float everage;
-	
+
 	void setup(int grade,int group,String name,int korean, int english, int math) {
-		this.setup(grade, group, name, korean, english, math, 0, 0);
-	}
-	
-	void setup(int grade,int group,String name,int korean, int english, int math,int total,float everage) {
 		this.grade=grade;
 		this.group=group;
 		this.name=name;
 		this.korean=korean;
 		this.english=english;
 		this.math=math;
-		this.total=korean+english+math;
-		this.everage=(float)(korean+english+math)/3;
 	}
+	
 	void show() {
 		System.out.println("학년 = " + this.grade);
 		System.out.println("반 = " + this.group);
@@ -32,9 +25,15 @@ public class Student {
 		System.out.println("국어 = " + this.korean);
 		System.out.println("영어 = " + this.english);
 		System.out.println("수학 = " +this.math);
-		System.out.println("총점= " +this.total);
-		System.out.println("평균= " +this.everage);
-		if(this.korean>=40&&this.english>=40&&this.math>=40&&this.everage>=60) {
+		int total=this.korean+this.english+this.math;
+		System.out.println("총점= " +total);
+		float everage=total/3f;
+		System.out.println("평균= " +everage);
+		boolean pass= this.korean>=40
+							&&this.english>=40
+							&&this.math>=40
+							&&everage>=60;
+		if(pass) {
 			System.out.println("통과");
 		}
 		else {
