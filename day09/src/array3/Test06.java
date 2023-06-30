a1ccaf70e996ee1f3d7312fe860fd7cbf0f53d29
wijaemin <minialp2002@naver.com>
2023-06-30 11:51:39 +0900

    빙고

--- day09/src/array3/Test06.java
+++ day09/src/array3/Test06.java
@@ -1,41 +1,40 @@
 package array3;
 
-import java.util.Random;
 
-public class Test06 {
-	public static void main(String[] args) {
-		/* 빙고판 만들기
-		 * 
-		 * 빙고판은 5x5 사이즈가 기본입니다
-		 * 빙고판은 정사각형이며 홀수 크기만 가능합니다.
-		 * 빙고판에는 1부터 랜덤한 위치에 배치됩니다
-		 * 한 칸에는 한번의 숫자만 배치 가능합니다.
-		 * System.out.println(bingo[j][i]);
-		 */
-		Random r= new Random();
-		
-		int [][]bingo =new int[5][5];
-		int num=1;
-		for(int j=0;j<bingo.length;j++) {
-			for(int i =0;i<bingo[j].length;i++) {
-				bingo[j][i]=num;
-				num++;
+	import java.util.Random;
+
+	public class Test06{
+		public static void main(String[] args) {
+			//빙고판 만들기
+			//= 랜덤한 위치에 1부터 숫자를 배치하는 형태
+			
+			//배열 생성
+			int size = 5;
+			
+			int[][] bingo = new int[size][size];
+			
+			//데이터 초기화
+			Random r = new Random();
+			for(int n=1; n <= size*size; n++) {
+				int x = r.nextInt(size);
+				int y = r.nextInt(size);
+				if(bingo[x][y] == 0) {//처음 넣는 자리라면
+					bingo[x][y] = n;
 				}
-		}
-		
-		
-		
-		for(int j=0;j<bingo.length;j++) {
-			for(int i =0;i<bingo[j].length;i++) {
-				System.out.print(bingo[j][i]);
-				System.out.print("\t");
+				else {
+					n--;
 				}
-			System.out.println();
+			}
+			
+			//출력
+			for(int i=0; i < size; i++) {
+				for(int k=0; k < size; k++) {
+					System.out.print(bingo[i][k]);
+					System.out.print("\t");
+				}
+				System.out.println("\n");
+			}
 		}
-		
-		
-		
-		
-		
 	}
-}
+
+
