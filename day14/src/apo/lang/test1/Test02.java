1752fbd7048d61c6e2b2315bdf3bcad0ce656663
wijaemin <minialp2002@naver.com>
2023-07-06 16:56:06 +0900

    Properties 활용

---
+++ day14/src/apo/lang/test1/Test02.java
@@ -0,0 +1,26 @@
+package apo.lang.test1;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class  Test02  {
+	public static void main(String[] args) {
+		//객체 생성
+		Properties prop = new Properties();
+		
+		//기능 사용
+		//-key=hello,value=world 인 데이터 저장
+		prop.setProperty("hello","world");
+		//-key=good,value=bye 인 데이터 저장
+		prop.setProperty("good","bye");
+		
+		//-객체의 요약 정보 =toString()
+		System.out.println(prop.toString());
+		//-key=hello인 데이터의 value를 꺼내어 출력
+		System.out.println(prop.getProperty("hello"));
+		
+		
+	}
+	
+
+}
