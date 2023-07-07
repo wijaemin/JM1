5a810c86a048bf744d78bc247b107e45ddfb6375
wijaemin <minialp2002@naver.com>
2023-07-07 17:40:54 +0900

    비밀번호 검사(긍정탐색방법)

--- day15/src/api/lang/string2/Test06.java
+++ day15/src/api/lang/string2/Test06.java
@@ -3,10 +3,12 @@ package api.lang.string2;
 public class Test06 {
 	public static void main(String[] args) {
 		//반드시 1개를 포함해야 되는 경우에 대한 검사
+		//(?=.*[a-z])반드시 들어가야함
+		//(?!.*[a-z])들어가면 안됨
 		
 		String password = "Khuser1234!";
 		
-		String regex ="[0-9]";
+		String regex ="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$])[A-Za-z0-9!@#$]{8,16}$";
 		
 		System.out.println(password.matches(regex));
 	}
