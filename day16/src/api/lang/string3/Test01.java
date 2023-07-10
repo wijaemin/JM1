829f12f4e52b613356c1f21c9a46f458a9314d03
wijaemin <minialp2002@naver.com>
2023-07-10 10:39:59 +0900

    문자열 연결 연산

---
+++ day16/src/api/lang/string3/Test01.java
@@ -0,0 +1,14 @@
+package api.lang.string3;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//문자열 합성
+		//=원하는 문자열을 만들기 위해 지속적인 연결을 하는 직업
+		
+		String line="이런 신발끈 개나리 진달래같은!!!!";
+		
+		line=line.replaceAll("(신발끈|개나리)", "***");
+		
+		System.out.println(line);
+	}
+}
