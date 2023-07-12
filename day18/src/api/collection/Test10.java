c9d326bd7b1945ac9442a9a2e79e2c37a5e7c9ca
wijaemin <minialp2002@naver.com>
2023-07-12 18:13:55 +0900

    주석 추가

--- day18/src/api/collection/Test10.java
+++ day18/src/api/collection/Test10.java
@@ -9,11 +9,13 @@ public class Test10 {
 	public static void main(String[] args) {
 		//ArrayList 말고 다른 List의 종류
 		//-ArrayList는 데이터가 배열 형태로 존재하는 리스트
-		//	-조회 성능이 우수함
-		//	-데이터 구조 변경이 느림
+		//		-조회 성능이 우수함
+		//		-데이터 구조 변경이 느림
 		//-LinkedList는 데이터가 연결 형태로 존재하는 리스트
-		//	-조회 성능이 다소 떨어짐
-		// -데이터 구조 변경 성능이 우수함
+		//		-조회 성능이 다소 떨어짐
+		//		-데이터 구조 변경 성능이 우수함
+		//-Vector는 과거 자바에서 사용하던 클래스
+		//		-조회 성능이 떨어져서 ArrayList로 교체하여 사용
 		List<String> a = new ArrayList<>();
 		List<String>b = new LinkedList<>();
 		
