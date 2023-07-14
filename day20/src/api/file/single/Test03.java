7f109f58821ca8b42c73f8c967b4b849120c2325
wijaemin <minialp2002@naver.com>
2023-07-14 16:46:04 +0900

    파일 입출력

---
+++ day20/src/api/file/single/Test03.java
@@ -0,0 +1,32 @@
+package api.file.single;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Test03 {
+	public static void main(String[] args) throws IOException {
+		File target =new File("D:/","origin.txt");//절대경로(absolute path)
+		FileInputStream stream =new FileInputStream(target);
+		
+//		int a=stream.read();
+//		System.out.println(a);
+//		a=stream.read();
+//		System.out.println(a);
+		
+		File target1 =new File("sample","copy.txt");//상대경로(relative path)
+		FileOutputStream stream1=new FileOutputStream(target1);
+		
+		while(true) {
+			
+			int a=stream.read();
+			if(a==-1) {
+				break;
+			}
+			stream1.write(a);
+		}
+		stream.close();
+		stream1.close();
+	}
+}
