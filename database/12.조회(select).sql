5a3494ac7af552f2a612ec239835de554b2464e1
wijaemin <137031368+wijaemin@users.noreply.github.com>
2023-07-20 13:32:52 +0900

    Update 12.조회(select).sql

--- database/12.조회(select).sql
+++ database/12.조회(select).sql
@@ -1,7 +1,9 @@
 /*
-	조회
+	조회(SELECT)
+	- 테이블의 데이터를 원하는 목적에 맞게 선택적으로 조회
 */
-drop table produck;
+
+-- 샘플 테이블
 create table product(
 no number primary key,
 name varchar2(30) not null,
@@ -22,130 +24,140 @@ insert into product values(8, '빠삐코', '아이스크림', 1000, '2019-12-01'
 insert into product values(9, '멘토스', '사탕', 1200, '2020-03-20', '2020-12-31');
 insert into product values(10, '오레오', '과자', 2100, '2019-06-01', '2020-06-01');
 
-commit;
---기본 조회
+
+-- 기본 조회
 select * from product;
 
---이름만 조회
+-- 이름만 조회
 select name from product;
-
---이름과 가격만 조회
+-- 이름과 가격만 조회
 select name, price from product;
+-- 없는 항목은 조회할 수 없다
+-- select name, price, point from product;
 
---없는 항목은 조회할 수 없다
---select name, price, point from product;
+-- 항목을 계산해서 조회할 수 있다.
+-- 날짜 계산 시 기본 처리 단위는 일이다
+select name, expire-made+1 from product;
 
---항목을 계산해서 조회할 수 있다.
---날짜 계산 시 기본 처리 단위는 일이다
-select name,expire-made+1 from product;
-
---조회한 항목에 표시될 이름을 지정할 수 있다(별칭 설정)
+-- 조회한 항목에 표시될 이름을 지정할 수 있다(별칭 설정)
 -- 별칭에 띄어쓰기가 있는 경우에는 쌍따옴표로 감싸서 처리
-select name "이름", expire-made +1 "유통기한" from product;
+select name "이름", expire-made+1 "유통기한" from product;
 
------------------------------------------------------------------
---조건(where) : 조획 결과를 원하는 방향으로 필터링
---------------------------------------------------------------------
-select * from product;
---2000원 이상인 상품 정보 조회
-select * from PRODUCT where price >=2000;;
+--------------------------------------------------
+-- 조건(where) : 조회 결과를 원하는 상황으로 필터링
+--------------------------------------------------
 
---1000원 이상 2000원 이하인 상품 정보 조회
-select * from product where price >=1000  and price <= 2000;
+-- <숫자 조건>
+-- 2000원 이상인 상품 정보 조회
+select * from product where price >= 2000;
+
+-- 1000원 이상 2000원 이하인 상품 정보 조회
+select * from product where price >= 1000 and price <= 2000;
 select * from product where price between 1000 and 2000;
 
---오라클에서는 같다가 =로 표현된다
-select * from product where price=1000;
-select * from product where price ! =1000;
+-- 오라클에서는 같다가 = 로 표현된다.
+select * from product where price = 1000;
+
+select * from product where price != 1000;
 select * from product where price <> 1000;
 
---<문자열 조건>
---문자열도 같음을 =로 처리한다
---유사검색은 like와 insetr()로 처리할 수 있다
--- 시작검색만큼은 반드시 like를 써야할 정도로 압도적인 차이가 발생
--- 나머지 성능은 전체적으로 instr가 우수하다
-select * from product where name='스크류바';
+-- <문자열 조건>
+-- 문자열도 같음을 = 로 처리한다
+-- 유사검색은 like와 instr()로 처리할 수 있다
+--		시작검색만큼은 반드시 like를 써야할 정도로 압도적인 차이가 발생
+--		나머지 성능은 전체적으로 instr이 우수하다
+--		복잡한 검색은 like로 가능
+select * from product;
+select * from product where name = '스크류바';
 
 select * from product where name like '스크%';
-select * from product where instr(name,'스크')=1;
+select * from product where instr(name, '스크') = 1;
 
-select * from product where name like'%이%';
-select * from product where instr(name,'이')>1;
+select * from product where name like '%이%';
+select * from product where instr(name, '이') > 0;
 
 select * from product where name like '오%오';
 
-
-select * from product ;
---(Q)과자와 아이스크림만 조회
-select * from product where type ='과자' or type= '아이스크림';
-select * from product where type in('과자','아이스크림');
---(Q) 이름에 '초코'가 들어간 상품 정보 조회
-select * from product where instr(name,'초코')>0;
-
---(Q) 이름이 '바나나'로 시작하는 상품 정보 조회
+-- (Q) 과자와 아이스크림만 조회
+select * from product where type = '과자' or type = '아이스크림';
+select * from product where type in ('과자', '아이스크림');
+-- (Q) 이름에 '초코'가 들어간 상품 정보 조회(instr이 좋음)
+select * from product where name like '%초코%';
+select * from product where instr(name, '초코') > 0;
+-- (Q) 이름이 '바나나'로 시작하는 상품 정보 조회(like가 좋음)
 select * from product where name like '바나나%';
-select * from product where instr(name,'바나나')=1;
---(Q) 이름이 '이'가 들어가지 않은 상품 정보 조회
-select * from product where instr(name,'이')=0;
---(Q) 가격이 1000원 이상인 과자를 조회
-select * from product where type= '과자' and price>=1000;
+select * from product where instr(name, '바나나') = 1;
+-- (Q) 이름에 '이'가 들어있지 않은 상품 정보 조회
+select * from product where name not like '%이%';
+select * from product where instr(name, '이') = 0;
+-- (Q) 가격이 1000원 이상인 과자를 조회
+select * from product where type = '과자' and price >= 1000;
 
---<날짜 조건>
---기본 형식은 yyyy-mm-dd hh24:mi:ss 이다
---문자열로 간주하고 조회할 수 있다(운영체제에 따라 작동여부 결정)
---문자열로 변환하고 싶다면 to_char() 사용
---2020년에 제조된 상품 조회
---날짜 추출용 함수인 extract()를 사용하여 원하는 항목을 추출
+-- <날짜 조건>
+-- 기본 형식은 yyyy-mm-dd hh24:mi:ss 이다
+-- 문자열로 간주하고 조회할 수 있다(운영체제에 따라 작동여부 결정)
+-- 문자열로 변환하고 싶다면 to_char() 사용
+-- 날짜 추출용 함수인 extract()를 사용하여 원하는 항목을 추출
 
---select * from product where made like '%2020%';
-select * from product where to_char(made,'yyyy') ='2020';
-select * from product where extract(year from made)=2020;
-select * from product where
-made between 
-	to_date('2020-01-01 00:00:00', 'yyyy-mm-dd hh24:mi:ss')
-	and
-	to_date('2020-12-31 23:59:59', 'yyyy-mm-dd hh24:mi:ss')
+-- 2020년에 제조된 상품 조회
+-- select * from product where made like '2020%';
+select * from product where to_char(made, 'yyyy') = '2020';
+select * from product where extract(year from made) = 2020;
+select * from product where 
+	made between 
+		to_date('2020-01-01 00:00:00', 'yyyy-mm-dd hh24:mi:ss')
+		and 
+		to_date('2020-12-31 23:59:59', 'yyyy-mm-dd hh24:mi:ss')
+		;
 
---(Q) 여름(6,7,8)월에 생산한 상품 정보 조회
-select * from product;
-select * from product where to_char(made,'mm') in ('06','07','08');
-select * from product where extract(month from made) between 6 and 8 ;
-
---(Q) 2019년 하반기에 생산한 상품 정보 조회
+-- (Q) 여름(6, 7, 8)월에 생산한 상품 정보 조회
+select * from product where to_char(made, 'mm') in ('06', '07', '08');
+select * from product where extract(month from made) between 6 and 8;
+	
+-- (Q) 2019년 하반기에 생산한 상품 정보 조회	
+-- 하반기 (07/01~12/31)
+select * from product 
+	where to_char(made, 'yyyy-mm') 
+		in ('2019-07', '2019-08', '2019-09', '2019-10', '2019-11', '2019-12');
 select * from product
-where to_char(made,'yyyy-mm') in ('2019-07','2019-08','2019-09','2019-10','2019-11','2019-12');
-
+	where extract(year from made) = 2019
+		and extract(month from made) between 7 and 12;
 select * from product
-	where extract(year from made)= 2019
-	and  extract(month from made) between 7 and 12;
+	where made between 
+		to_date('2019-07-01 00:00:00', 'yyyy-mm-dd hh24:mi:ss') 
+		and 
+		to_date('2019-12-31 23:59:59', 'yyyy-mm-dd hh24:mi:ss');
 
-select * from product where made between
-to_date('2019-07-01 00:00:00', 'yyyy-mm-dd hh24:mi:ss')
-and
-to_date('2019-12-31 23:59:59', 'yyyy-mm-dd hh24:mi:ss');
-
-
---(Q) 2020년 부터 현재까지 생산한 상품 정보 조회
-select * from product where made between
-to_date('2020-01-01 00:00:00', 'yyyy-mm-dd hh24:mi:ss')
-and
-sysdate;
---(Q)최근 1년간 생산한 상품 정보 조회
---오라클 날짜는 기본 계산 단위가 (일)이다
---따라서 1년전은 sysdate-365이다
-
-select * from product where made between 
-sysdate -365 and sysdate ;
-
---(응용)시간까지 고려(시작일 00시부터 종료일 23시 59분까지)
+-- (Q) 2020년 부터 현재까지 생산한 상품 정보 조회
 select * from product
-	where made between
+	where made between 
+		to_date('2020-01-01 00:00:00', 'yyyy-mm-dd hh24:mi:ss') 
+		and 
+		sysdate;
+
+-- (Q) 최근 1년간 생산한 상품 정보 조회
+-- 오라클 날짜는 기본 계산 단위가 (일) 이다
+-- 따라서 1년전은 sysdate - 365이다.
+select * from product 
+	where made between sysdate-365 and sysdate;
+
+-- (응용) 시간까지 고려(시작일 00시부터 종료일 23시59분까지)
+select * from product 
+	where made between 
 	to_date(
-	to_char(sysdate-2000,'yyyy-mm-dd') ||' '|| '00:00:00',
-	'yyyy-mm-dd hh24:mi:ss')
-	AND 
-	to_date(to_char(sysdate,'yyyy-mm-dd') || ' ' || '23:59:59',
-	'yyyy-mm-dd hh24:mi:ss')
-	;
+		to_char(sysdate-365, 'yyyy-mm-dd') || ' ' || '00:00:00',
+		'yyyy-mm-dd hh24:mi:ss'
+	)
+	and 
+	to_date(
+		to_char(sysdate, 'yyyy-mm-dd') || ' ' || '23:59:59', 
+		'yyyy-mm-dd hh24:mi:ss'
+	);
 
 
+	
+	
+	
+	
+	
+	
