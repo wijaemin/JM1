9137879c8725a329cdd3551d4b215a6f8eb74d2b
wijaemin <minialp2002@naver.com>
2023-07-21 18:45:04 +0900

    과제 1

---
+++ jdbc01/src/jdbc2/Test02.java
@@ -0,0 +1,32 @@
+package jdbc2;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class Test02 {
+	public static void main(String [] args) {
+		int bookId = 11;
+		String bookTitle = "바꿀제목";
+		double bookPrice = 123.99;
+
+		String sql = "update book set book_title = ?, book_price = ? where book_id = ?";
+		Object[] data = {bookTitle, bookPrice, bookId};
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		dataSource.setUsername("c##kh");
+		dataSource.setPassword("KH");
+		
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		
+		int result = jdbcTemplate.update(sql, data);
+		System.out.println("result = " + result);
+		
+		if (result > 0) System.out.println("Success");
+		else System.out.println("Fail");
+		
+		
+		
+	}
+}
