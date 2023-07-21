758c177e0b386b2b87232bced1d86c281749dbe5
wijaemin <minialp2002@naver.com>
2023-07-21 14:27:24 +0900

    jdbc 등록 연습 (account)

--- jdbc01/src/jdbc/Test05.java
+++ jdbc01/src/jdbc/Test05.java
@@ -9,16 +9,20 @@ public class Test05 {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		// 이수근 3 17 1000000 17000000
-		String account_owner=sc.next();
-		int account_period=sc.nextInt();
-		int account_count=sc.nextInt();
-		int account_payment =sc.nextInt();
-		int account_balance =sc.nextInt();
+		System.out.print("예금주 : ");
+		String accountOwner=sc.nextLine();
+		System.out.print("예금기간(년) : ");
+		int accountPeriod=sc.nextInt();
+		int accountCount=0;
+		System.out.print("1회 예금액 : ");
+		int accountPayment =sc.nextInt();
+		int accountBalance =0;
 		
+		//DB 처리
 		String sql="insert into account(account_owner, account_period, account_count,"
 				+ "account_payment, account_balance) values(?, ?, ?, ?, ?)";
 		
-		Object[] data = {account_owner, account_period,account_count,account_payment,account_balance};
+		Object[] data = {accountOwner, accountPeriod,accountCount,accountPayment,accountBalance};
 		
 		DriverManagerDataSource dataSource =new DriverManagerDataSource();
 		dataSource.setDriverClassName("oracle.jdbc.OracleDriver"); //DB종류
@@ -31,8 +35,9 @@ public class Test05 {
 		
 		jdbcTemplate.update(sql,data);
 		
-		System.out.println("미션 컴플리트");
+		System.out.println("ㅗ");
 		
+
 		
 		
 		
