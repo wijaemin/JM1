85bf047614a7bf7da7abaee7c2ad072ecfab54f2
wijaemin <137031368+wijaemin@users.noreply.github.com>
2023-07-24 00:20:02 +0900

    Create 16.서브쿼리(subquery).sql

---
+++ database/16.서브쿼리(subquery).sql
@@ -0,0 +1,23 @@
+--------------------------------------------------
+-- 서브쿼리(sub query)
+--------------------------------------------------
+-- 여러 개의 구문을 순차적으로 실행하도록 만드는 기술
+
+-- (ex) 가장 비싼 금액의 상품 정보를 조회
+select max(price) from product;
+select * from product where price = 3000;
+
+-- 위의 예제를 서브쿼리로 변환하면 아래와 같다
+select * from product where price = (
+	select max(price) from product
+);
+
+-- (Q) 가장 최근에 제조된 상품 정보
+select * from product where made = (
+	select max(made) from product
+);
+
+-- (Q) 가격이 가장 저렴한 책 정보
+select * from book where book_price = (
+	select min(book_price) from book
+);
