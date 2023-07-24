0f5ee926cb002ac163371151128928c48983e7a2
wijaemin <minialp2002@naver.com>
2023-07-24 09:15:27 +0900

    백준

--- BAEKJOON/src/Main.java
+++ BAEKJOON/src/Main.java
@@ -5,8 +5,6 @@ public class Main {
 
        Scanner sc = new Scanner(System.in);
 
-	Scanner sc= new Scanner(System.in);
-	
 	String s=sc.next();
 	for(char c='a';c<='z';c++) {
 		System.out.print(s.indexOf(c)+ " ");
