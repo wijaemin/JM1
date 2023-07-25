4fd7161c0c72776c7aa19974f9921d49d5490db4
wijaemin <minialp2002@naver.com>
2023-07-25 14:36:13 +0900

    백준 풀이

--- BAEKJOON/src/Main.java
+++ BAEKJOON/src/Main.java
@@ -2,26 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-    	Scanner sc=new Scanner(System.in);
-    	
-    	long N=sc.nextLong();
-    	
-    	while(N!=1){
-    		int num=2;
-    		Long num1=N/num;
-    		System.out.println(num);
-    		N=num1;
-    	}
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
+        Scanner sc = new Scanner(System.in);
+        
+        int count=sc.nextInt();
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<count;i++) {
+        	int num=sc.nextInt();
+        	for(int j=1;j<=num;j++) {
+        		if(num%j==0) count2+=1;
+        	}
+        	if(count2==2) count1+=1;
+        }
+        System.out.println(count1);
 	}
 }
