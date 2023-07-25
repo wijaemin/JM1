96718244ce31eda4560669e9d24bb053075a9e34
wijaemin <minialp2002@naver.com>
2023-07-26 00:57:06 +0900

    Customer 수정 프로그램

--- jdbc03/src/jdbc/app/Test00_Customer1.java
+++ jdbc03/src/jdbc/app/Test00_Customer1.java
@@ -5,11 +5,12 @@ import jdbc.dto.CustomerDto;
 
 public class Test00_Customer1 {
 
+	//고객 수정 프로그램
 	public static void main(String[] args) {
 		CustomerDto dto=new CustomerDto();
+		dto.setCustomerNo(7);
 		dto.setCustomerId("sotired123");
 		dto.setCustomerContact("010-1122-3344");
-		dto.setCustomerNo(21);
 		CustomerDao dao=new CustomerDao();
 		boolean result=dao.update(dto);
 		if(result){
