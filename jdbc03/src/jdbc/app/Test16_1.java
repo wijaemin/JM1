98461a108380e11918b5b3f320dd2992a0940023
wijaemin <minialp2002@naver.com>
2023-07-25 17:31:52 +0900

    게시판 조회,페이지 수만큼 조회

---
+++ jdbc03/src/jdbc/app/Test16_1.java
@@ -0,0 +1,30 @@
+package jdbc.app;
+
+import java.util.List;
+
+import jdbc.dao.BoardDao;
+import jdbc.dto.BoardDto;
+
+public class Test16_1 {
+
+	public static void main(String[] args) {
+		int boardNo=1;
+		
+		BoardDao dao =new BoardDao();
+		BoardDto dto=dao.slectOne(boardNo);
+		List<BoardDto> list=dao.selectList();
+		
+		if(dao==null) {
+			System.out.println("찾는 게시글 목록은 없습니다");
+		}
+		else {
+			System.out.println(dto.getBoardNo());
+			System.out.println(dto.getBoardTitle());
+			System.out.println(dto.getBoardContent());
+			System.out.println(dto.getBoardWriter());
+			System.out.println(dto.getBoardContent());
+		}
+
+	}
+
+}
