43e90186a98a889680fe25006cd5e3b93ff3b073
wijaemin <minialp2002@naver.com>
2023-07-26 10:01:53 +0900

    멤버 조회 수정

--- jdbc03/src/jdbc/app/Test21.java
+++ jdbc03/src/jdbc/app/Test21.java
@@ -1,7 +1,5 @@
 package jdbc.app;
 
-import java.util.List;
-
 import jdbc.dao.MemberDao;
 import jdbc.dto.MemberDto;
 
@@ -11,17 +9,13 @@ public class Test21 {
 		String memberId="testuser12345";
 		MemberDao dao=new MemberDao();
 		MemberDto dto=dao.selectOne(memberId);
-		List<MemberDto> list=dao.selectList();
 		
 		if(dto==null) {
 			System.out.println("조회하려는 아이디가 없음");
 		}
 		else {
 			System.out.println(dto.getMemberId());
-			System.out.println(dto.getMemberPw());
 			System.out.println(dto.getMemberNickname());
-			System.out.println(dto.getMemberBirth());
-			System.out.println(dto.getMemberEmail());
 			System.out.println(dto.getMemberContact());
 			System.out.println(dto.getMemberLevel());
 			System.out.println(dto.getMemberPoint());
