d228bf32d9946e752ab087cfe39553fb50b5745b
wijaemin <minialp2002@naver.com>
2023-07-27 13:56:01 +0900

    spring06 생성

---
+++ spring06board/src/main/java/com/kh/spring06/controller/BoardController.java
@@ -0,0 +1,22 @@
+package com.kh.spring06.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.spring06.dao.BoardDao;
+import com.kh.spring06.dto.BoardDto;
+
+@RestController
+public class BoardController {
+	@Autowired
+	private BoardDao dao;
+	
+	@RequestMapping
+	public String insert(@ModelAttribute BoardDto dto) {
+		dao.insert(dto);
+		return "등록 완료!";
+		
+	}
+}
---
+++ spring06board/src/main/java/com/kh/spring06/dao/BoardDao.java
@@ -0,0 +1,24 @@
+package com.kh.spring06.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring06.dto.BoardDto;
+
+@Repository
+public class BoardDao {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public void insert(BoardDto dto) {
+		String sql="intset into board(board_no, board_title, "
+				+ "board_content, board_writer, board_readcount "
+				+ "values(board_seq.nextval,?,?,?,?)";
+		Object[] data= {dto.getBoardTitle(), dto.getBoardContent(), 
+				dto.getBoardWriter(), dto.getBoardReadcount()};
+		jdbcTemplate.update(sql,data);
+		
+		
+	}
+}
