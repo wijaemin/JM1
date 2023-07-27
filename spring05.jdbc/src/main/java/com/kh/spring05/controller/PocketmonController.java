89cc282b9c0dd70de47249902c84eb62288cf495
wijaemin <minialp2002@naver.com>
2023-07-27 17:01:24 +0900

    PocketmonController 수정

--- spring05.jdbc/src/main/java/com/kh/spring05/controller/PocketmonController.java
+++ spring05.jdbc/src/main/java/com/kh/spring05/controller/PocketmonController.java
@@ -3,6 +3,7 @@ package com.kh.spring05.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring05.dao.PocketmonDao;
@@ -20,5 +21,23 @@ public class PocketmonController {
 	public String insert(@ModelAttribute PocketmonDto dto) {
 		dao.insert(dto);
 		return "포켓몬 등록 완료!";
+	}
+	@RequestMapping("/update")
+	public String update(@ModelAttribute PocketmonDto dto) {
+		boolean result =dao.update(dto);
+		
+		if(result) {
+			return "포켓몬스터 정보 변경 완료!";
+		}
+		else return "해당 번호는 존재하지 않습니다";
+		
+	}
+	@RequestMapping("/delete")
+	public String delete(@RequestParam int no) {
+		boolean result=dao.delete(no);
+		
+		if(result) return "포켓몬스터 삭제 완료";
+		else return "해당 번호는 존재하지 않아요";
+	
 	}
 }
