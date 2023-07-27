8fa999cb677c3eb0c0345b65c44718f559a7bf03
wijaemin <minialp2002@naver.com>
2023-07-27 17:01:09 +0900

    BoardController 수정

--- spring06board/src/main/java/com/kh/spring06/controller/BoardController.java
+++ spring06board/src/main/java/com/kh/spring06/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.kh.spring06.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring06.dao.BoardDao;
@@ -18,5 +19,17 @@ public class BoardController {
 		dao.insert(dto);
 		return "등록 완료!";
 		
+	}
+	@RequestMapping("/update")
+	public String update(@ModelAttribute BoardDto dto) {
+		boolean result=dao.update(dto);
+		if(result) return "게시판 정보 변경 완료";
+		else return "없는 게시판 번호 입니다";
+	}
+	@RequestMapping("/delete")
+	public String delete(@RequestParam int boardNo) {
+		boolean result=dao.delete(boardNo);
+		if(result) return "게시판 정보 삭제 완료";
+		else return "게시판에 없는 번호입니다";
 	}
 }
