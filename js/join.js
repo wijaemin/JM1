b2d6a13b958d38e316d7e6fa7a53fc624a49578f
wijaemin <minialp2002@naver.com>
2023-09-07 16:15:28 +0900

    회원 가입 화면의 분할 및 추가 처리 구현

--- js/join.js
+++ js/join.js
@@ -1,128 +1,142 @@
-function checkMemberId(){
-    var input=document.querySelector("[name=memberId]");
-    var regex=/^[a-z][a-z0-9-_]{4,19}$/;
-    var isValid=regex.test(input.value);
+$(function(){
 
-    input.classList.remove("success","fail","fail2");
-    if(isValid){
-        //아이디 중복검사 코드 및 성공 실패
-        input.classList.add("success");
-        return true;
-    }
-    else{
-        input.classList.add("fail");
-        return false;
-    }
 
-}
-function checkMemberPw(){
-    var input=document.querySelector("[name=memberPw]");
-    var regex=/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$])[A-Za-z0-9!@#$]{8,16}$/;
-    var isValid=regex.test(input.value);
+    var status={
+        id:false,
+        pw:false,
+        pwCheck:false,
+        nickname:false,
+        email:false,
+        contact:false,
+        birth:false,
+        address:false,
+        ok:function(){
+            return this.id && this.pw && this.pwCheck &&this.nickname && this.email && this.contact && this.birth && this.address;
+        }
+    };
 
-//     input.classList.remove("success","fail");
-//    if(isValid){
-//         input.classList.add("success");
-//         return true;
-//    }
-//    else{
-//         input.classList.add("fail");
-//         return false;
-//    }
-   input.classList.add(isValid ? "success" : "fail");
-   return isValid;
-}
-function checkMemberPw2(){
-    var input1=document.querySelector("[name=memberPw]");
-    var input2=document.querySelector("#password-check");
 
-    input2.classList.remove("success","fail","fail2");
-    if(input1.value.length==0){//비밀번호 미작성
-        input2.classList.add("fail2");
-        return false;
-    }
-    else if(input1.value==input2.value){//비밀번호 일치
-        input2.classList.add("success");
-        return true;
 
-    }
-    else{//비밀번호 불일치
-        input2.classList.add("fail");
-        return false;
-    }
-}
-function checkMemberNickname(){
-    var input=document.querySelector("[name=memberNickname]");
-    var regex=/^[가-힣0-9]{1,10}$/;
-    // var regex=/^[ㄱ-ㅎㅏ-ㅣ가-힣0-9]{2,10}$/;
-    var isValid=regex.test(input.value);
+    $("[name=memberId]").blur(function(){
+        var regex=/^[a-z][a-z0-9-_]{4,19}$/;
+        var inputId=$(this).val();
+        
+        var isValid=regex.test(inputId);
+        $(this).removeClass("success fail");
+        $(this).addClass(isValid ? "success" : "fail");
+        status.id=isValid;
+    });
 
-    input.classList.remove("success","fail","fail2");
-    if(isValid){
-        input.classList.add("success");
-        return true;
-    }
-    else{
-        input.classList.add("fail");
-        return false;
-    }
-}
-function checkMemberEmail(){
-    var input=document.querySelector("[name=memberEmail]");
-    var regex=/^(.*?)@(.*?)$/;
+    $("[name=memberPw]").blur(function(){
+        var regex=/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$])[A-Za-z0-9!@#$]{8,16}$/;
+        var inputPw=$(this).val();
+        
+        var isValid=regex.test(inputPw);
+        $(this).removeClass("success fail");
+        $(this).addClass(isValid ? "success" : "fail");
+        status.pw=isValid;
+    });
 
-    var isValid= input.value.length == 0 || regex.test(input.value);
-    input.classList.remove("success","fail");
-    input.classList.add(isValid ? "success" : "fail");
-    return isValid;
-}
-function checkMemberContact(){
-    var input = document.querySelector("[name=memberContact]");
-    var regex = /^010[1-9][0-9]{7}$/;
+    $("#password-check").blur(function(){
+        var inputPw=$("[name=memberPw]").val();
+        var checkPw=$(this).val();
 
-    var isValid = input.value.length == 0 || regex.test(input.value);
+        var isValid=inputPw==checkPw;
 
-    input.classList.remove("success","fail");
-    input.classList.add(isValid ? "success" : "fail");
-    return isValid;
-}
-function checkMemberBirth(){
-    var input =document.querySelector("[name=memberBirth]");
-    var regex=/^(19[0-9]{2}|20[0-9]{2})-(((0[13578]|1[02])-(0[1-9]|1[0-9]|2[0-9]|3[01]))|((0[469]|11)-(0[1-9]|1[0-9]|2[0-9]|30))|((02)-(0[1-9]|1[0-9]|2[0-9])))$/;
-    
-    var isValid = input.value.length == 0 || regex.test(input.value);
-    input.classList.remove("success","fail");
-    input.classList.add(isValid ? "success" : "fail");
-    return isValid;
-}
-function checkMemberAddress(){
-    var input1 = document.querySelector("[name=memberPost]");
-    var input2 = document.querySelector("[name=memberAddr1]");
-    var input3 = document.querySelector("[name=memberAddr2]");
+        $(this).removeClass("success fail fail2");
+        if(inputPw.length==0){
+            $(this).addClass("fail2");
+            status.pwCheck=false;
 
-    var isBlank = input1.value.length == 0 && input2.value.length == 0 && input3.value.length == 0;
-    var isFill = input1.value.length > 0 && input2.value.length > 0 && input3.value.length > 0;
+        }
+        else if(isValid){
+            $(this).addClass("success");
+            status.pwCheck=true;
+        }
+        else{
+            $(this).addClass("fail");
+            status.pwCheck=false;
+        }
+    });
 
-    var isValid = isBlank || isFill;
+    $("[name=memberNickname]").blur(function(){
+        var regex=/^[가-힣0-9]{1,10}$/;
+        var inputNickname=$(this).val();
+        
+        var isValid=regex.test(inputNickname);
+        $(this).removeClass("success fail fail2");
+        if(isValid){
+            $(this).addClass("success");
+            status.nickname=true;
+        }
+        else{
+            $(this).addClass("fail");
+            status.nickname=false;
+        }
+    });
+    $("[name=memberEmail]").blur(function(){
+        var regex=/^(.*?)@(.*?)$/;
+        var inputEmail=$(this).val();
+        
+        var isValid=$(this).val().length==0 || regex.test(inputEmail);
+        $(this).removeClass("success fail");
+        $(this).addClass(isValid ? "success" : "fail");
+        status.email=isValid;
+    });
+    $("[name=memberContact]").blur(function(){
+        var regex = /^010[1-9][0-9]{7}$/;
+        var inputContact=$(this).val();
+        
+        var isValid=$(this).val().length==0 || regex.test(inputContact);
+        $(this).removeClass("success fail");
+        $(this).addClass(isValid ? "success" : "fail");
+        status.contact=isValid;
+    });
+    $("[name=memberBirth]").blur(function(){
+        var regex=/^(19[0-9]{2}|20[0-9]{2})-(((0[13578]|1[02])-(0[1-9]|1[0-9]|2[0-9]|3[01]))|((0[469]|11)-(0[1-9]|1[0-9]|2[0-9]|30))|((02)-(0[1-9]|1[0-9]|2[0-9])))$/;
+        var inputBirth=$(this).val();
+        
+        var isValid=$(this).val().length==0 || regex.test(inputBirth);
+        $(this).removeClass("success fail");
+        $(this).addClass(isValid ? "success" : "fail");
+        status.birth=isValid;
+    });
 
-    input1.classList.remove("success","fail");
-    input2.classList.remove("success","fail");
-    input3.classList.remove("success","fail");
 
-    input1.classList.add(isValid ? "success" : "fail");
-    input2.classList.add(isValid ? "success" : "fail");
-    input3.classList.add(isValid ? "success" : "fail");
 
-    return isValid;
-}
-function checkForm(){
-    var r1=checkMemberId();
-    var r2=checkMemberPw();
-    var r3=checkMemberPw2();
-    var r4=checkMemberNickname();
-    var r5=checkMemberEmail();
-    var r6=checkMemberContact();
-    var r7=checkMemberBirth();
-    var r8=checkMemberAddress();
-    return r1&&r2&&r3&&r4&&r5&&r6&&r7&&r8;
-}
+    $("[name=memberPost],[name=memberAddr1],[name=memberAddr2]").blur(function(){
+        var inputPost=$("[name=memberPost]").val();
+        var inputAddr1=$("[name=memberAddr1]").val();
+        var inputAddr2=$("[name=memberAddr2]").val();
+
+        var isBlank = inputPost.length==0 && inputAddr1.length==0 && inputAddr2.length ==0;
+        var isFill = inputPost.length>0 && inputAddr1.length>0 && inputAddr2.length >0;
+
+        var isValid= isBlank || isFill;
+
+        $("[name=memberPost]").removeClass("success fail");
+        $("[name=memberAddr1]").removeClass("success fail");
+        $("[name=memberAddr2]").removeClass("success fail");
+
+        $("[name=memberPost]").addClass(isValid ? "success" : "fail");
+        $("[name=memberAddr1]").addClass(isValid ? "success" : "fail");
+        $("[name=memberAddr2]").addClass(isValid ? "success" : "fail");
+
+        status.address=isValid;
+    });
+
+    $(".join-form").submit(function(e){
+        // $(".form-input").blur();//버튼 눌렀을 때 메세지 뜨게 하기
+        //status를 확인
+        console.table(status);
+        // console.log(status.ok());
+        if(status.ok()==false){
+            e.preventDefault();
+        }
+        
+
+    });
+
+
+
+    });
