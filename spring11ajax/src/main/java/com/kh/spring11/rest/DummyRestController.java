0791145864be7f90290679cdba17f6fbd9a8e737
wijaemin <minialp2002@naver.com>
2023-09-08 11:09:39 +0900

    스프링 11에서 작업

---
+++ spring11ajax/src/main/java/com/kh/spring11/rest/DummyRestController.java
@@ -0,0 +1,45 @@
+package com.kh.spring11.rest;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//CORS를 해제하기 위한 설정(Annotation)
+//@CrossOrigin//전부 다 허용(위험)
+@CrossOrigin(origins= {"http://192.168.30.55:5500"})
+@RestController //@Controller + @ResponseBody
+public class DummyRestController {
+	
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello spring ajax";
+	}
+	
+	//Rest Controller에서는 내가 전해줄 데이터가 반환형이 된다
+	//-자동으로 Spring에서 JSON형태로 변환하여 반환
+	//-변환을 담당하는 라이브러리는 jackson-databind
+	@RequestMapping("/lotto")
+	public Set<Integer> lotto() {
+		Random r= new Random();
+		Set<Integer> set = new TreeSet<>();
+		while(set.size()<6) {
+			int n=r.nextInt(45)+1;
+			set.add(n);
+		}
+		return set;
+	}
+	
+	
+	
+}
+
+
+
+
+
+
--- spring11ajax/src/test/java/com/kh/spring11/Spring11ajaxApplicationTests.java
+++ spring11ajax/src/test/java/com/kh/spring11/Spring11ajaxApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kh.project;
+package com.kh.spring11;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
