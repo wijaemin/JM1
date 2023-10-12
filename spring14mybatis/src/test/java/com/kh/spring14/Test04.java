2cc6850dfb939a45fcbbdfa624beb42a9856e225
wijaemin <minialp2002@naver.com>
2023-10-12 12:54:24 +0900

    상세,등록,수정 해보기

---
+++ spring14mybatis/src/test/java/com/kh/spring14/Test04.java
@@ -0,0 +1,35 @@
+package com.kh.spring14;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.spring14.dto.PocketmonDto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class Test04 {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void test() {
+		PocketmonDto dto=new PocketmonDto();
+		dto.setNo(1);
+		dto.setName("건강챙겨");
+		dto.setType("위딸기");
+		
+		int count = sqlSession.update("pocketmon.edit",dto);
+		boolean result=count>0;
+		log.debug("result={}",result);
+	}
+}
+
+
+
+
+
+
