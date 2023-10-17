5c26db8c426939fcea40d3fca152495ec0b4a1a8
wijaemin <minialp2002@naver.com>
2023-10-17 16:21:08 +0900

    첨부파일 이메일로 보내기

--- spring18-sequrity/src/main/java/com/kh/spring18/service/EmailServiceImpl.java
+++ spring18-sequrity/src/main/java/com/kh/spring18/service/EmailServiceImpl.java
@@ -1,17 +1,19 @@
 package com.kh.spring18.service;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,15 +32,23 @@ public class EmailServiceImpl implements EmailService {
 	  helper.setSubject("kh에 온걸 환영");
 	  
 	  ClassPathResource resource = new ClassPathResource("templates/celebration.html");
-	  File target= resource.getFile();
-	  Scanner sc= new Scanner(target);
-	  StringBuffer buffer =new StringBuffer();
-	  while(sc.hasNextLine()) {
-		  buffer.append(sc.nextLine());
-	  }
-	  sc.close();
+	  Document doc =Jsoup.parse(resource.getFile(),"UTF-8");
 	  
-	  helper.setText(buffer.toString(),true);
+	  //수신자 표시
+	  Element target= doc.getElementById("target");
+	  target.text(email);
+	  
+	  //로그인 링크 작성
+	  Element link = doc.getElementById("login-link");
+//	  link.attr("href","/secure/login");//절대 경로지만 안됨(다른 사이트에서 실행하기 때문)
+	  
+	  //주소를 현재 상황에 맞게 생성하는 도구 사용
+	  String href=ServletUriComponentsBuilder.fromCurrentContextPath()
+	  							.path("/secure/login")
+	  							.build()
+	  							.toUriString();//문자열로 바꿔라
+	  link.attr("href",href);
+	  helper.setText(doc.toString(),true);
 	  
 	  sender.send(message);
 	}
