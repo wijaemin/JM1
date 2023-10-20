5ef4c4d77f12e69c2f9323c9909f89687032997e
wijaemin <minialp2002@naver.com>
2023-10-20 10:35:32 +0900

    기본 웹소켓 예제

---
+++ spring20websocket/src/main/java/com/kh/spring20/controller/WebSocketViewController.java
@@ -0,0 +1,20 @@
+package com.kh.spring20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebSocketViewController {
+	
+	@RequestMapping("/")
+	public String home() {
+//		return"/WEB-INF/views/home.jsp";
+		return "home";
+	}
+	
+	@RequestMapping("/default")
+	public String defaultServer() {
+//		return "/WEB-INF/views/default.jsp";
+		return "default";
+	}
+}
