435e07f8b161a7e3f6cdb3fc97f8f505db1ed072
wijaemin <minialp2002@naver.com>
2023-10-20 15:30:32 +0900

    회원 로그인 로그아웃, 회원 전용 웹소켓 구현

--- spring20websocket/src/main/java/com/kh/spring20/controller/WebSocketViewController.java
+++ spring20websocket/src/main/java/com/kh/spring20/controller/WebSocketViewController.java
@@ -1,7 +1,15 @@
 package com.kh.spring20.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.spring20.dto.MemberDto;
 
 @Controller
 public class WebSocketViewController {
@@ -27,5 +35,33 @@ public class WebSocketViewController {
 	public String group() {
 		return "group";
 		
+	}
+	//임시 로그인 처리
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@PostMapping("/login")
+	public String login(@ModelAttribute MemberDto memberDto, HttpSession session) {
+		MemberDto findDto = sqlSession.selectOne("member.find",memberDto);
+		if (findDto !=null) {
+			boolean pwMatch = findDto.getMemberPw().equals(memberDto.getMemberPw());
+			if(pwMatch) {
+				session.setAttribute("name", findDto.getMemberId());//아이디
+				session.setAttribute("level", findDto.getMemberLevel());//등급
+			}
+		}
+		
+		return "redirect:/";
+	}
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("name");
+		session.removeAttribute("level");
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/member")
+	public String member() {
+		return "member";
 	}
 }
