531c2c6541f534cfedfc1656229bb1e83c1bc19b
wijaemin <minialp2002@naver.com>
2023-11-06 15:14:10 +0900

    Json 생성 항목 제거 처리

--- spring22rest/src/main/java/com/kh/spring22/dto/PocketmonDto.java
+++ spring22rest/src/main/java/com/kh/spring22/dto/PocketmonDto.java
@@ -2,6 +2,8 @@ package com.kh.spring22.dto;
 
 import javax.validation.constraints.Pattern;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,6 +25,7 @@ public class PocketmonDto {
 			nullable = false,example = "전기")
 	private String type;
 	
+	@JsonIgnore
 	@Schema(hidden =true)
 	public boolean isEmpty() {
 		return no == 0 && name == null && type == null;
